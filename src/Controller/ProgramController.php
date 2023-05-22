aa79e912214ac791e3292794b179ee70f322c9b1
Urzaag <remi.meslin.gm@gmail.com>
2023-05-22 11:41:03 +0200

    webpack encore + stuff

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/program/', name: 'program_')]
+#[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
     #[Route('/', name: 'index')]
@@ -17,7 +17,7 @@ class ProgramController extends AbstractController
         ]);
     }
 
-    #[Route('{id}', name: 'showById', requirements: ['id'=>'\d+'], methods: ['GET'])]
+    #[Route('/{id}', name: 'showById', requirements: ['id'=>'\d+'], methods: ['GET'])]
     public function show(int $id): Response
     {
         return $this->render('program/show.html.twig', [
